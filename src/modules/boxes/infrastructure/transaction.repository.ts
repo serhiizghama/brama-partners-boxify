a2e08cb77c9bd386ddc7db5a6094de2a9565c68e
serhiizghama <zmrser@gmail.com>
2025-10-21 15:20:01 +0700

    feat: add product management functionality to boxes module with new DTOs and service methods

--- src/modules/boxes/infrastructure/transaction.repository.ts
+++ src/modules/boxes/infrastructure/transaction.repository.ts
@@ -1,28 +1,34 @@
-import { EntityManager } from 'typeorm';
+import { EntityManager, Repository } from 'typeorm';
 import { BoxEntity } from '../../../domain/boxes/entities/box.entity';
 import { ProductEntity } from '../../../domain/products/entities/product.entity';
 import { CreateBoxDto } from '../dto/create-box.dto';
 
 export class TransactionRepository {
-  constructor(private readonly manager: EntityManager) {}
+  private readonly boxRepo: Repository<BoxEntity>;
+  private readonly productRepo: Repository<ProductEntity>;
+
+  constructor(private readonly manager: EntityManager) {
+    this.boxRepo = this.manager.getRepository(BoxEntity);
+    this.productRepo = this.manager.getRepository(ProductEntity);
+  }
 
   async createBox(dto: CreateBoxDto): Promise<BoxEntity> {
-    const repo = this.manager.getRepository(BoxEntity);
-    const entity = repo.create(dto);
-    return await repo.save(entity);
+    const entity = this.boxRepo.create(dto);
+    return await this.boxRepo.save(entity);
+  }
+
+  async findBoxById(id: string): Promise<BoxEntity | null> {
+    return await this.boxRepo.findOne({
+      where: { id },
+      relations: ['products'],
+    });
   }
 
   async findProductById(id: string): Promise<ProductEntity | null> {
-    const repo = this.manager.getRepository(ProductEntity);
-    return await repo.findOne({ where: { id } });
+    return await this.productRepo.findOne({ where: { id } });
   }
 
-  async updateProductBoxId(productId: string, boxId: string): Promise<ProductEntity | null> {
-    const repo = this.manager.getRepository(ProductEntity);
-    const entity = await repo.findOne({ where: { id: productId } });
-    if (!entity) return null;
-
-    entity.box_id = boxId;
-    return await repo.save(entity);
+  async updateProductBoxId(id: string, box_id: string | null): Promise<void> {
+    await this.productRepo.update(id, { box_id });
   }
 }
